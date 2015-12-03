4a9efa211917d2b079a184f2209712b52ba115c4
MikelAndMendizabal <birrismo@yahoo.es>
2015-12-03 02:50:27 +0100

    first stat bar animated

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ $(document).ready(function() {
 $("#menu_video").css("background", "#6afbff");
 
 var pictures = ["#pic1", "#pic2" ,"#pic3","#pic4", "#pic5" ,"#pic6", "#pic7", "#pic8" ,"#pic9","#pic10" ];
+var stats1=[40, 66.9, 94.6, 87.5,87.7,10,32,94.3 ];
+
 $("#pic1").fadeIn();
+$("#stat1").animate({
+           width: '+='+stats1[0]+'px'
+       });
 $("#pic2").fadeOut();
 $("#pic3").fadeOut();
 $("#pic4").fadeOut();
